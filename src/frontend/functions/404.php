a3122131f96a697364d68463bf09179a5a9952b8
bmichal123 <michalb.92@o2.pl>
2017-11-07 22:52:20 +0100

    Create 404.php

---
+++ src/frontend/functions/404.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * Render 404 page
+ */
+function render404($config) {
+	header('HTTP/1.1 404 Not Found');
+	$html = file_get_contents(__DIR__ . "/../www/404.html");
+	$html = str_replace("{{base_url}}", $config['server']['base_url'], $html);
+	
+	print $html;
+}
