3ae9698c84cbb2f476275011dec8b9999727fa30
bmichal123 <michalb.92@o2.pl>
2017-12-16 22:50:16 +0100

    Add files via upload

--- src/frontend/functions/login.php
+++ src/frontend/functions/login.php
@@ -14,19 +14,20 @@ function loginGET($data, $config, $status = '') {
     else if(isset($_COOKIE['alert'])) {
         $html = str_replace('{{alert.class}}', 'alert-success', $html);
         $html = str_replace('{{alert.content}}', htmlspecialchars($_COOKIE['alert']), $html);
-        removeCookie('alert');
     }
     
     else if(isset($_COOKIE['alert_danger'])) {
         $html = str_replace('{{alert.class}}', 'alert-danger', $html);
         $html = str_replace('{{alert.content}}', htmlspecialchars($_COOKIE['alert_danger']), $html);
-        removeCookie('alert_danger');
     }
     
     else {
         $html = str_replace('{{alert.class}}', '', $html);
         $html = str_replace('{{alert.content}}', '', $html);
-    } 
+    }
+    
+    removeCookie('alert');
+    removeCookie('alert_danger');
     
     print $html;
 }
@@ -40,17 +41,29 @@ function loginPOST($data, $config) {
 
     $url = $config['service']['users'] . '/login/' . $loginData['login'];
     $ret = remoteCall($url, 'POST', json_encode($loginData));
+    
 
+    
     if($ret['status'] == 200) {
-        $tokenData = json_decode($ret['body'], true);
-        setcookie('login', $loginData['login'], time()+60*60*24*365, '/');
-        setcookie('token', $tokenData['token'], time()+60*60*24*365, '/');
+        if( strlen($loginData['login']) > 0 ||
+            strlen($loginData['password']) > 0
+        ) {
+            $tokenData = json_decode($ret['body'], true);
+            setcookie('login', $loginData['login'], time()+60*60*24*365, '/');
+            setcookie('token', $tokenData['token'], time()+60*60*24*365, '/');
+        }
         
-        redirectTo('/');
+        else {
+            addCookie('alert_danger', $config['login']['403']);
+        }
     }
+    
+    else if($ret['status'] == 403) {
+        addCookie('alert_danger', $config['login']['403']);
+    }
+    
+    redirectTo('/');
 }
-
-
 
 /*
  * Logout user
